c9a088838c8d50ce917c86d155d1b6d47a76dfb5
unknown <jonnathan.rodriguez@cun.edu.co>
2023-04-07 16:57:09 -0500

    Migrando el sistemas

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const Server = require('./models/server')    // Ya esta la instancia
+const Server = require('./models/server')    // Ya esta la instancia del server
 
 const server = new Server();
 
 server.listen();
 
-// este es la ruta xd C:\Users\WINDOWS10\Documents\Jonnathan Green\Cursos\N ode.js\Seccion8_RestServer
+// este es la ruta del nuevo sistema xd C:\Users\WINDOWS10\Documents\Jonnathan Green\Cursos\N ode.js\Seccion8_RestServer
