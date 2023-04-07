d92729d83f8cf4fc5ff4b13ae441988c3cc5d3b0
unknown <jonnathan.rodriguez@cun.edu.co>
2023-04-07 16:44:15 -0500

    Corrigiendo la creacion de usuarios en la base de datos

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const server = new Server();
 
 server.listen();
 
-//C:\Users\WINDOWS10\Documents\Jonnathan Green\Cursos\N ode.js\Seccion8_RestServer
+// este es la ruta xd C:\Users\WINDOWS10\Documents\Jonnathan Green\Cursos\N ode.js\Seccion8_RestServer
